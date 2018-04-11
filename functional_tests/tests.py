c85cd177e9609ba41188846a09dee148e93adf5f
Jiezhi <Jiezhi.G@gmail.com>
2018-04-11 16:32:45 +0800

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,12 +6,12 @@ Created on 4/10/18
 
 Reference: 
 """
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
@@ -23,8 +23,8 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_start(self):
-        self.browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.live_server_url)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
@@ -36,6 +36,8 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_now_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,8 +48,22 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_now_in_list_table('1: Buy peacock feathers')
         self.check_for_now_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the test!')
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
 
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lsits/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_id('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
--- lists/views.py
+++ lists/views.py
@@ -8,12 +8,13 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        print('create:', new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
+    return render(request, 'home.html')
+
+
+def view_list(request):
     items = Item.objects.all()
-    print(items)
-
-    return render(request, 'home.html', {
+    return render(request, 'list.html', {
         'items': items,
     })
