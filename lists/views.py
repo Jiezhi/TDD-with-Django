9502521bf0b9739a966ad0271180a4095429c295
Jiezhi <Jiezhi.G@gmail.com>
2018-04-11 13:47:59 +0800

    refactor

--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do</title></html>')
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
