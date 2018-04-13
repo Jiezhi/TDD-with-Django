2bb481b13126eee82d5e302c7423d817baff418f
Jiezhi <Jiezhi.G@gmail.com>
2018-04-13 14:57:50 +0800

    ExistingListItemForm

--- lists/forms.py
+++ lists/forms.py
@@ -7,20 +7,12 @@ Created on 4/12/18
 Reference: 
 """
 from django import forms
+from django.core.exceptions import ValidationError
 from lists.models import Item
 
 EMPTY_LIST_ERROR = 'You cannot have an empty list item'
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
-
-# class ItemForm(forms.Form):
-#     item_text = forms.CharField(
-#         widget=forms.fields.TextInput(
-#             attrs={
-#                 'placeholder': 'Enter a to-do item',
-#                 'class': 'form-control input-lg',
-#             }
-#         )
-#     )
 
 class ItemForm(forms.ModelForm):
     class Meta:
@@ -41,3 +33,19 @@ class ItemForm(forms.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
